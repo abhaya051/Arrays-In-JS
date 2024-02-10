d5d8c080c9753f7a2f7fdf68437de073144a7209
Suraj Gautam <surajgautam783792@gmail.com>
2024-02-10 23:38:04 +0530

    Arrays

---
+++ index.js
@@ -0,0 +1,95 @@
+// ***************-------------- Arrays
+
+// let marks = [52, 69, 78, 69, 20, 63];
+// console.log(marks);
+// console.log(marks.length);
+
+// Task First Arrays No To Rohan Name Student
+
+// ------------************************-----------------
+// Arrays Indices Or Index
+// let data = [25, 256, 98, 369, 41, 2589];
+// console.log(data); // It Will Return Arrays
+
+// console.log(data[0]); // If You Wanted to Put First Value Of Array You Will Write This
+
+// data[0] = "Rohan"; // It Will Change A 0 Indices Or Index Value
+
+// console.log(data);
+
+// ------------************************-----------------
+
+// Without Using Looping Over An Arrays
+// let color = ["Black", "White", "Yellow", "Blue", "Green", "Purple"];
+// console.log(color[0]);
+// console.log(color[1]);
+// console.log(color[2]);
+// console.log(color[3]);
+
+//  Using Looping Over An Arrays (For-Loops)
+// let color = ["Black", "White", "Yellow", "Blue", "Green", "Purple"];
+// for (let i = 0; i < color.length; i++) {
+//   console.log(color[i]);
+// }
+
+//  Using Looping Over An Arrays (For-of-Loops)
+// let color = ["Black", "White", "Yellow", "Blue", "Green", "Purple"];
+// for (let i of color) {
+//   console.log(i);
+// }
+
+// Methods of Arrays
+//  First Is Push Method
+// let veggies = ["Potato", "Tomato", "Beans"];
+// console.log(veggies); // Print Veggies
+// veggies.push("Onion"); // Adding Only Onion In Arrays
+// veggies.push("Onion", "Peas", "Capsicum"); // Adding So Many Veggies In Arrays
+// console.log(veggies);
+
+// //  Second Is POP Method
+// let veggies = ["Potato", "Tomato", "Beans"];
+// console.log(veggies); // Print Veggies
+// veggies.pop();
+// console.log(veggies);
+// // console.log(veggies);
+
+//  Third Is toString Method
+// let veggies = ["Potato", "Tomato", "Beans"];
+// console.log(veggies); // Print Veggies
+// console.log(veggies.toString());
+// // console.log(veggies);
+
+// Fourth Is Concat Method
+// let veggies = ["Potato", "Tomato", "Beans"];
+// let fruits = ["Litchi", "Apple", "Mango"];
+// console.log(veggies.concat(fruits)); // It Will Concat Both Arrays
+
+// Fifth Is Unshift Method   ----(lIKE A PUSH)
+// let fruits = ["Litchi", "Apple", "Mango"];
+// console.log(fruits);
+// fruits.unshift("Grapes");
+// console.log(fruits);
+
+// Sixth Is Shift Method ---(lIKE A POP)
+// let fruits = ["Litchi", "Apple", "Mango"];
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+// Seventh Is Slice Method ---(Putting A Piece Of Arrays)
+// let fruits = ["Litchi", "Apple", "Mango"];
+// console.log(fruits.slice(1, 3));
+
+// Sixth Is Splice Method ---(add,remove and replace )
+// let num = [1, 2, 8, 9, 54, 663];
+// Add Splice
+// num.splice(2, 0, "Rohan");
+// console.log(num);
+
+// Delete Splice
+// num.splice(3, 1);
+// console.log(num);
+
+// Replace Splice
+// num.splice(3, 0, "Rohan");
+// console.log(num);
